6686d4be682558ae5d13daee91d4e6f99ff92379
Ser7ho <serzzhh@gmail.com>
2018-06-20 00:05:17 +0300

    добавляет валидацию хэш-тегов

--- js/pictures.js
+++ js/pictures.js
@@ -126,7 +126,7 @@ var imgUploadPreview = imgUploadOverlay.querySelector('.img-upload__preview');
 
 var onImgUploadEscPress = function (evt) {
   var ESC_KEYCODE = 27;
-  if (evt.keyCode === ESC_KEYCODE) {
+  if (evt.keyCode === ESC_KEYCODE && !evt.target.classList.contains('text__hashtags') && !evt.target.classList.contains('text__description')) {
     closeImgUpload();
   }
 };
@@ -254,5 +254,139 @@ var onPlusResizeClick = function () {
   setValueResize(true);
   resizeScale();
 };
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// hashtags validity ------------------------------------------------------------------------------------------------------
+
+(function () {
+
+  var REGEXP = /[^\S]+/g;
+  var invalidities = [];
+
+  var addInvalidity = function (message) {
+    invalidities.push(message);
+  };
+  var getInvalidities = function () {
+    return invalidities.join('. \n');
+  };
+  var convertToArray = function (input) {
+    var arrayHashtags = input.value.split(REGEXP);
+    for (var i = 0; i < arrayHashtags.length; i++) {
+      if (arrayHashtags[i] === '') {
+        arrayHashtags.splice(i);
+      }
+    }
+    return arrayHashtags;
+  };
+  var checkValidity = function (input) {
+    invalidities.length = 0;
+    for (var i = 0; i < hashtagsValidityChecks.length; i++) {
+
+      var isInvalid = hashtagsValidityChecks[i].isInvalid(convertToArray(input));
+      if (isInvalid) {
+        addInvalidity(hashtagsValidityChecks[i].invalidityMessage);
+      }
+
+      var requirementElement = hashtagsValidityChecks[i].element;
+      if (requirementElement) {
+        if (isInvalid) {
+          requirementElement.classList.add('invalid');
+          requirementElement.classList.remove('valid');
+        } else {
+          requirementElement.classList.remove('invalid');
+          requirementElement.classList.add('valid');
+        }
+
+      }
+    }
+  };
+
+  function checkInput(input) {
+
+    checkValidity(input);
+
+    if (invalidities.length === 0 && input.value === '') {
+      input.setCustomValidity('');
+    } else {
+      var message = getInvalidities();
+      input.setCustomValidity(message);
+    }
+  }
+
+  var hashtagsValidityChecks = [
+    {
+      isInvalid: function (arrayHashtags) {
+        return arrayHashtags.length > 5;
+      },
+      invalidityMessage: 'укажите не больше пяти хэш-тегов',
+      element: document.querySelector('.text__hashtags + .text__requirements li:nth-child(1)')
+    },
+    {
+      isInvalid: function (arrayHashtags) {
+        for (var i = 0; i < arrayHashtags.length - 1; i++) {
+          for (var j = i + 1; j < arrayHashtags.length; j++) {
+            if (arrayHashtags[i].toLowerCase() === arrayHashtags[j].toLowerCase() && arrayHashtags[i] !== '') {
+              return true;
+            }
+          }
+        }
+        return false;
+      },
+      invalidityMessage: 'один и тот же хэш-тег не может быть использован дважды',
+      element: document.querySelector('.text__hashtags + .text__requirements li:nth-child(2)')
+    },
+    {
+      isInvalid: function (arrayHashtags) {
+        if (arrayHashtags.length !== 0) {
+          for (var i = 0; i < arrayHashtags.length; i++) {
+            if ((arrayHashtags[i].length < 2 && arrayHashtags[i] !== '') || arrayHashtags[i].length > 20) {
+              return true;
+            }
+          }
+        }
+        return false;
+      },
+      invalidityMessage: 'длина одного хэш-тега от 2 до 20 символов, включая решётку',
+      element: document.querySelector('.text__hashtags + .text__requirements li:nth-child(3)')
+    },
+    {
+      isInvalid: function (arrayHashtags) {
+        if (arrayHashtags.length !== 0) {
+          for (var i = 0; i < arrayHashtags.length; i++) {
+            if (arrayHashtags[i].charAt(0) !== '#' && arrayHashtags[i] !== '') {
+              return true;
+            }
+          }
+        }
+        return false;
+      },
+      invalidityMessage: 'хэш-тег начинается с символа # (решётка)',
+      element: document.querySelector('.text__hashtags + .text__requirements li:nth-child(4)')
+    },
+    {
+      isInvalid: function (arrayHashtags) {
+        for (var i = 0; i < arrayHashtags.length; i++) {
+          if (~arrayHashtags[i].indexOf('#', 1) && arrayHashtags[i] !== '') {
+            return true;
+          }
+        }
+        return false;
+      },
+      invalidityMessage: 'хэш-теги разделяются пробелами',
+      element: document.querySelector('.text__hashtags + .text__requirements li:nth-child(5)')
+    }
+  ];
+
+  var inputHashtags = document.querySelector('.text__hashtags');
+
+  inputHashtags.addEventListener('keyup', function () {
+    checkInput(inputHashtags);
+  });
+  inputHashtags.addEventListener('change', function () {
+    inputHashtags.value = inputHashtags.value.split(REGEXP).join(' ');
+  });
+
+})();
 
 // ---------------------------------------------------------------------------------------------------------------------
